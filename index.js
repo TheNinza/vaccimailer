5abc4b72d02a03cd0b3d5fb12dd395161a29b663
Nikhil Kumar Gupta <niks.a3198@gmail.com>
2021-05-10 18:47:06 +0530

    added files

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
+const got = require("got");
 const dotenv = require("dotenv");
-// const nodemailer = require("nodemailer");
-const https = require("https");
-const express = require("express");
+const nodemailer = require("nodemailer");
+const http = require("http");
 
 dotenv.config();
 
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.GMAIL_EMAIL,
-//     pass: process.env.GMAIL_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_EMAIL,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
-// const sendEmail = (text) => {
-//   transporter.sendMail(
-//     {
-//       from: process.env.GMAIL_EMAIL,
-//       to: process.env.GMAIL_EMAIL_REC,
-//       subject: "Cowin available",
-//       text,
-//     },
-//     (err, info) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(info);
-//       }
-//     }
-//   );
-// };
+const sendEmail = (text) => {
+  transporter.sendMail(
+    {
+      from: process.env.GMAIL_EMAIL,
+      to: process.env.GMAIL_EMAIL_REC,
+      subject: "Cowin available",
+      text,
+    },
+    (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    }
+  );
+};
 
-// sendEmail("first email from dummy server");
+// sendEmail("first email from server");
 
 let dataArray = [];
 
@@ -47,39 +47,30 @@ const vaccinationDate = `${d.getDate() + 1}-0${
 const apiUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pincode}&date=${vaccinationDate}`;
 
 function getData() {
-  https
-    .get(apiUrl, (resp) => {
-      let data = "";
+  got(apiUrl)
+    .then((response) => {
+      dataArray = JSON.parse(response.body).sessions;
+      // console.log(dataArray);
 
-      // A chunk of data has been received.
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
+      let availableSlots = dataArray.filter((obj) => obj.min_age_limit < 45);
 
-      // The whole response has been received. Print out the result.
-      resp.on("end", () => {
-        dataArray = JSON.parse(data).sessions;
-        // console.log(dataArray);
-        let availableSlots = dataArray.filter((obj) => obj.min_age_limit < 45);
+      // Play some sound if slots are available and get notified
 
-        // Play some sound if slots are available and get notified
-
-        if (availableSlots.length > 0) {
-          console.log(availableSlots);
-          // sendEmail("Vaccination available in your city");
-          clearInterval(myInt);
-        } else {
-          const date = new Date();
-          console.log(
-            date.toTimeString(),
-            "no available slots available for",
-            vaccinationDate
-          );
-        }
-      });
+      if (availableSlots.length > 0) {
+        console.log(availableSlots);
+        sendEmail("Vaccination available in your city");
+        clearInterval(myInt);
+      } else {
+        const date = new Date();
+        console.log(
+          date.toTimeString(),
+          "no available slots available for",
+          vaccinationDate
+        );
+      }
     })
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
+    .catch((error) => {
+      console.log(error);
     });
 }
 
@@ -94,15 +85,9 @@ const myInt = setInterval(() => {
   getData();
 }, 30000);
 
-// creating a local server for fetching data-array
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send(dataArray);
-});
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log("Started server");
-});
+http
+  .createServer(function (req, res) {
+    res.write(JSON.stringify(dataArray)); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(process.env.PORT || 8000);
