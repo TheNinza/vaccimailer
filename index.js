2f60c169e48046f3f0e9f8f0c20692696f2f1110
kailash360 <kailashkejriwal21@gmail.com>
2021-10-02 00:26:52 +0530

    replaced 4-tab with 2-tab indentation

--- index.js
+++ index.js
@@ -8,28 +8,28 @@ const app = express()
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.GMAIL_EMAIL,
-        pass: process.env.GMAIL_PASS,
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_EMAIL,
+    pass: process.env.GMAIL_PASS,
     },
 });
 
 const sendEmail = (text) => {
-    transporter.sendMail({
-            from: process.env.GMAIL_EMAIL,
-            to: process.env.GMAIL_EMAIL_REC,
-            subject: "Cowin available",
-            text,
-        },
-        (err, info) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(info);
-            }
-        }
-    );
+  transporter.sendMail({
+    from: process.env.GMAIL_EMAIL,
+    to: process.env.GMAIL_EMAIL_REC,
+    subject: "Cowin available",
+    text,
+  },
+  (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  }
+  );
 };
 
 // sendEmail("first email from server");
@@ -38,7 +38,7 @@ let dataArray = [];
 
 // add pincode and date of vaccination
 const pincode = "731101";
-const d = new Date();
+const d = new Date()
 
 // vaccination date is the next day in following format
 const vaccinationDate = d.getMonth() < 9 ? `${d.getDate()}-0${d.getMonth() + 1}-${d.getFullYear()}` : `${d.getDate()}-${d.getMonth()}-${d.getFullYear()}`;
@@ -49,31 +49,31 @@ const apiUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/fin
 
 const getData = async() => {
 
-    try {
-        const result = await axios.get(apiUrl);
-        const response = result.data
-        dataArray = response.sessions;
-        // console.log(dataArray);
+  try {
+    const result = await axios.get(apiUrl);
+    const response = result.data
+    dataArray = response.sessions;
+    // console.log(dataArray);
 
-        let availableSlots = dataArray.filter((obj) => obj.min_age_limit < 45);
+    let availableSlots = dataArray.filter((obj) => obj.min_age_limit < 45);
 
-        // Play some sound if slots are available and get notified
+    // Play some sound if slots are available and get notified
 
-        if (availableSlots.length > 0) {
-            console.log(availableSlots);
-            sendEmail(JSON.stringify(availableSlots, null, 2));
-            clearInterval(myInt);
-        } else {
-            const date = new Date();
-            console.log(
-                date.toTimeString(),
-                "no available slots available for",
-                vaccinationDate
-            );
-        }
-    } catch (error) {
-        console.log(error);
+    if (availableSlots.length > 0) {
+      console.log(availableSlots);
+      sendEmail(JSON.stringify(availableSlots, null, 2));
+      clearInterval(myInt);
+    } else {
+      const date = new Date();
+      console.log(
+      date.toTimeString(),
+      "no available slots available for",
+      vaccinationDate
+    );
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 console.log("API URL:", apiUrl);
@@ -87,19 +87,19 @@ getData();
 
 // running function every 30 seconds
 const myInt = setInterval(() => {
-    getData();
+  getData();
 }, 100000);
 
 
 
 //created a server using express 
 app.get("/", (req, res) => {
-    res.write(JSON.stringify(dataArray, null, 2)); //write a response to the client
-    res.end(); //end the response
+  res.write(JSON.stringify(dataArray, null, 2)); //write a response to the client
+  res.end(); //end the response
 })
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 })
